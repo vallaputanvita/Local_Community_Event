1f1906872b84396c753f691aef1e5cad4f479163
vallaputanvita <159677672+vallaputanvita@users.noreply.github.com>
2025-05-27 19:23:20 +0530

    Create Stream.java

---
+++ java/Stream.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StreamAPIExample {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> evens = numbers.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
+        System.out.println("Even numbers: " + evens);
+    }
+}
