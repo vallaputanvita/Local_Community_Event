00959bdde25d991fdea037871b8cd1c2dee35d0d
vallaputanvita <159677672+vallaputanvita@users.noreply.github.com>
2025-05-27 19:11:33 +0530

    Create Stringreversal.java

---
+++ java/Stringreversal.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class StringReversal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        StringBuilder reversed = new StringBuilder(input).reverse();
+        System.out.println("Reversed string: " + reversed);
+    }
+}
