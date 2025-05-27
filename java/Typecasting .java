438ef42f15118047c1bfda19c6cfec738b3d6f42
vallaputanvita <159677672+vallaputanvita@users.noreply.github.com>
2025-05-27 12:22:39 +0530

    Create Typecasting .java

---
+++ java/Typecasting .java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Main
+  {
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      double b=sc.nextDouble();
+      int c=(int)b;
+      double d=a;
+      System.out.println("Integer :" +a);
+      System.out.println("double :" +b);
+      System.out.println("doubletointeger :" +c);
+      System.out.println("Integertodouble :" +d);
+    }
+  }
