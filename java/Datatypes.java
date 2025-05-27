35629923a8b4676e50bf63a1d97a09487856d7cb
vallaputanvita <159677672+vallaputanvita@users.noreply.github.com>
2025-05-27 12:15:31 +0530

    Create Datatypes.java

---
+++ java/Datatypes.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Datatypes
+  {
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      float b=sc.nextFloat();
+      double c=sc.nextDouble();
+      char d=sc.next().charAt(0);
+      boolean e=sc.nextBoolean();
+      System.out.println("Int: " + a);
+      System.out.println("Float: " + b);
+      System.out.println("Double: " + c);
+      System.out.println("Char: " + d);
+      System.out.println("Boolean: " + e);
+    }
+  }
